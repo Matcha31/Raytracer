4b0ebe22e4aa7cccb844342fb0f82f8761fe30ce
matcha31 <salom.berger@epita.fr>
2025-02-23 23:39:22 +0100

    original commit : vetcor 3 class

---
+++ src/vector3.cpp
@@ -0,0 +1,38 @@
+#ifndef VECTOR3_H
+#define VECTOR3_H
+
+#include <iostream>
+#include <cmath>
+
+class Vector3 {
+public:
+    float x, y, z;
+
+    // Constructeurs
+    Vector3() : x(0), y(0), z(0) {}
+    Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
+
+    // Opérations vectorielles
+    Vector3 operator+(const Vector3& v) const { return Vector3(x + v.x, y + v.y, z + v.z); }
+    Vector3 operator-(const Vector3& v) const { return Vector3(x - v.x, y - v.y, z - v.z); }
+    Vector3 operator*(float s) const { return Vector3(x * s, y * s, z * s); }
+    Vector3 operator/(float s) const { return Vector3(x / s, y / s, z / s); }
+
+    // Produit scalaire et produit vectoriel
+    float dot(const Vector3& v) const { return x * v.x + y * v.y + z * v.z; }
+    Vector3 cross(const Vector3& v) const { return Vector3(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x); }
+
+    // Normalisation
+    Vector3 normalize() const {
+        float norm = std::sqrt(x * x + y * y + z * z);
+        return (*this) / norm;
+    }
+
+    // Affichage pour le débogage
+    friend std::ostream& operator<<(std::ostream& out, const Vector3& v) {
+        out << "(" << v.x << ", " << v.y << ", " << v.z << ")";
+        return out;
+    }
+};
+
+#endif // VECTOR3_H
